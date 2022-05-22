3fc9ea32cdc9ecc90e7fe9d55452bd85b821ecd4
komaf5436 <k.3624.t.aw@gmail.com>
2022-05-22 08:25:45 +0000

    引数を定義 戻り値

--- greeting.rb
+++ greeting.rb
@@ -1,6 +1,7 @@
 # メソッドの定義をする場合はdef ~ endで実行する処理内容を記述する
 def greeting(name)
   "Hello,#{name}!" #この行のnameは、引数で渡すname 変数展開のため、''→""へ変更
+  "Good morning, #{name}!" #def ~ end内の最終結果が戻り値という
 end
 
 # 'John'を引数として渡す
