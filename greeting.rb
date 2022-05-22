ae8dbc642a841781c017d4e329dc132a0aa116ae
komaf5436 <k.3624.t.aw@gmail.com>
2022-05-22 08:16:15 +0000

    メソッドを定義 引数

--- greeting.rb
+++ greeting.rb
@@ -1,6 +1,7 @@
 # メソッドの定義をする場合はdef ~ endで実行する処理内容を記述する
-def greeting
-  'Hello'
+def greeting(name)
+  "Hello,#{name}!" #この行のnameは、引数で渡すname 変数展開のため、''→""へ変更
 end
 
-puts greeting
+# 'John'を引数として渡す
+puts greeting('John')
